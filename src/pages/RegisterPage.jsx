89089c2bda7612f3d22074d4fb40f0313fb44f93
MercyNJ <mercinjuguna@gmail.com>
2024-06-28 14:07:19 +0000

    Ran another build to include env variables

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,13 @@
 import {useState} from "react";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+/*const BASE_URL = process.env.REACT_APP_BASE_URL;*/
 
 export default function RegisterPage() {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	async function register(ev) {
 		ev.preventDefault();
-		const response = await fetch(`${BASE_URL}/register`, {
+		const response = await fetch(`/register`, {
 			method: 'POST',
 			body: JSON.stringify({username, password}),
 			headers: {'Content-Type':'application/json'},
