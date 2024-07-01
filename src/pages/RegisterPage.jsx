44b4e4064f5554dcf192863d4b369c55bf03338f
MercyNJ <mercinjuguna@gmail.com>
2024-07-01 17:30:30 +0000

    build for production

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,33 +1,42 @@
-import {useState} from "react";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function RegisterPage() {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	async function register(ev) {
-		ev.preventDefault();
-		const response = await fetch(`/register`, {
-			method: 'POST',
-			body: JSON.stringify({username, password}),
-			headers: {'Content-Type':'application/json'},
-		});
-		if (response.status === 200) {
-			alert('Registration Successful');
-		} else {
-			alert('Registration Failed');
-		}
-	}
-        return (
-                <form className="register" onSubmit={register}>
-		   <h1>Register</h1>
-                     <input type="text"
-		            placeholder="username"
-		            value={username}
-		            onChange={ev => setUsername(ev.target.value)}/>
-                     <input type="password"
-		            placeholder="password"
-		            value={password}
-		            onChange={ev => setPassword(ev.target.value)}/>
-                     <button>Register</button>
-                </form>
-        );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [registrationSuccessful, setRegistrationSuccessful] = useState(false);
+
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch(`/register`, {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      setRegistrationSuccessful(true);
+    } else {
+      alert('Registration Failed');
+    }
+  }
+
+  if (registrationSuccessful) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input type="text"
+        placeholder="username"
+        value={username}
+        onChange={ev => setUsername(ev.target.value)} />
+      <input type="password"
+        placeholder="password"
+        value={password}
+        onChange={ev => setPassword(ev.target.value)} />
+      <button>Register</button>
+    </form>
+  );
 }
+
